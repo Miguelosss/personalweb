6fddd9910584357e40b8b3909d4075f5d4cfaf8d
Miguelosss <heckmiika@hotmail.com>
2017-04-10 01:03:41 +0300

    progress

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+var junction_image = document.getElementById("junction");
+var improx_image = document.getElementById("improx");
 
+junction_image.onclick = function(){
+  var info = document.getElementById("info")
+  info.innerHTML = "<h2>Junction</h2><p>Junction is a hackathon</p>";
+}
+
+
+
+function changeText(image){
+  if(image.id == "junction"){
+    document.getElementByID("info").innerHTML = "<h2>Junction</h2><p>Junction is a hackathon</p>"
+  }
+}
